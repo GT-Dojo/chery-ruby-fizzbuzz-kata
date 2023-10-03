fae6f4d62d19c83bbb497c37dcf602d633faa65b
Chery <CC94@users.noreply.github.com>
2023-10-03 13:25:05 +0800

    Update fizzbuzz_practice.rb to display result via for loop

--- fizzbuzz_practice.rb
+++ fizzbuzz_practice.rb
@@ -7,21 +7,27 @@
 # Then delete the other one, along with this comment!
 
 def global_answer
-  6 * 9
-end
-
-class Hiker
-
-  def print_number(input)
-    if input % 15 == 0
-       'FizzBuzz'
-    elsif input%5 == 0
-      'Buzz'
-    elsif input%3 == 0
-      'fizz'
-    else
-      input
-    end
+    6 * 9
   end
+  
+  class Hiker
+  
+    def print_number(input)
+      if input % 15 == 0
+         'FizzBuzz'
+      elsif input%5 == 0
+        'Buzz'
+      elsif input%3 == 0
+        'fizz'
+      else
+        input
+      end
+    end
 
-end
+    hiker = Hiker.new
+    for a in 1..100 do
+        puts(hiker.print_number(a))
+    end
+  
+  end
+  
